29b14b4193f12929014e1ed11eb67e8b82a11039
Jake Beamish <jakebeamish@gmail.com>
2023-01-19 08:37:35 +0000

    Still flapping, breakdown API request URL

--- sketch.js
+++ sketch.js
@@ -6,7 +6,14 @@ function setup() {
 }
 
 function getData() {
-    console.log('getting data...')
+    console.log('getting data...');
+    let lat = 52;
+    let lon = 13;
+    let startDate = 1955-12-14;
+    let endDate = 1970-02-01;
+    let datatime = 'hourly';
+    let parameter = 'temperature_2m';
+    let url = `https://archive-api.open-meteo.com/v1/archive?${lat}&${lon}&${startDate}&${endDate}&${datatime}=${parameter}`;
     loadJSON('https://archive-api.open-meteo.com/v1/archive?latitude=52.52&longitude=13.41&start_date=2022-12-14&end_date=2023-01-13&hourly=temperature_2m', gotData)
 }
 
