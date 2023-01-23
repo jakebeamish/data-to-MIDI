fd2bfd20ac37e9f53be3a3beff0d0b8f26ba516e
Jake Beamish <jakebeamish@gmail.com>
2023-01-23 08:58:06 +0000

    Add navigator.geolocation.getCurrentLocation()

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
+let coordinates, latitude, longitude;
+
+const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  function success(pos) {
+    const crd = pos.coords;
+  
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+
+    document.getElementById("latitude").value = crd.latitude;
+    document.getElementById("longitude").value = crd.longitude;
+  }
+  
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(success, error, options);
+
+}
+
 function getData() {
     let startDate = document.getElementById("start-date").value;
     let endDate = document.getElementById("end-date").value;
-    let latitude = document.getElementById("latitude").value;
-    let longitude = document.getElementById("longitude").value;
-    let granularity = "hourly";
-    let parameter = document.getElementById("parameter").value;
 
     if (Date.parse(new Date(startDate)) > Date.parse(new Date(endDate))) {
         alert("Start date is after end date. Swapping them around")
@@ -12,6 +37,12 @@ function getData() {
         startDate = endDate;
         endDate = swap;
     }
+
+    latitude = document.getElementById("latitude").value;
+    longitude = document.getElementById("longitude").value;
+
+    let granularity = "hourly";
+    let parameter = document.getElementById("parameter").value;
 
     buildURL(startDate, endDate, latitude, longitude, granularity, parameter)
 }
@@ -27,3 +58,6 @@ function buildURL(startDate, endDate, latitude, longitude, granularity, paramete
 
     console.log(openMeteoURL);
 }
+
+
+  
